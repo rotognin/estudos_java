02aad6905a380e82f9ce19f000f23a0322057932
Rodrigo Tognin <rotog@outlook.com>
2021-04-19 22:15:32 -0300

    Implementando trabalho com arquivo texto

--- estudo01.java
+++ estudo01.java
@@ -4,7 +4,8 @@
  */
 
 import java.io.File;
-import java.io.FileNotFoundException;
+//import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class estudo01
@@ -14,8 +15,15 @@ public class estudo01
         funcoes.escrever("Hello World");
 
         // Ler um arquivo txt
-
-
-
+        try
+        {
+            File arquivo = new File("texto01.txt");
+            if (arquivo.createNewFile()){
+                funcoes.escrever("Arquivo criado con sucesso!");
+            }
+        } catch (IOException exc) {
+            funcoes.escrever("Exceção ocorrida: " );
+            exc.printStackTrace();
+        }
     }
 }
