6f7dbfc3c4737365d6cda27176b7953f537155aa
Rodrigo Tognin <rotog@outlook.com>
2021-04-21 10:53:35 -0300

    Escrita no arquivo

--- estudo01.java
+++ estudo01.java
@@ -4,12 +4,37 @@
  */
 
 import java.io.File;
+import java.io.FileWriter;
 //import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class estudo01
 {
+    public static void processarArquivo(String arq)
+    {
+        // Abrir o arquivo para escrita
+        try
+        {
+            FileWriter arquivo = new FileWriter(arq);
+            Scanner texto = new Scanner(System.in);
+            String mensagem;
+            funcoes.escrever("Digite o texto a ser escrito no arquivo:");
+            mensagem = texto.nextLine();
+            texto.close();
+
+            if (!mensagem.isEmpty()){
+                arquivo.write(mensagem);
+                funcoes.escrever("Texto escrito no arquivo.");
+            }
+           
+            arquivo.close();
+        }
+        catch (IOException exc){
+            funcoes.escrever(exc.getMessage());
+        }
+    }
+
     public static void main(String[] args)
     {
         funcoes.escrever("Hello World");
@@ -17,9 +42,18 @@ public class estudo01
         // Ler um arquivo txt
         try
         {
-            File arquivo = new File("texto01.txt");
+            String arq = "texto01.txt";
+            File arquivo = new File(arq);
+
             if (arquivo.createNewFile()){
-                funcoes.escrever("Arquivo criado con sucesso!");
+                funcoes.escrever("Arquivo criado com sucesso!");
+            } else {
+                funcoes.escrever("Arquivo já existente.");
+            }
+
+            if (arquivo.exists()){
+                // A partir daqui, trabalhar com o arquivo
+                processarArquivo(arq);
             }
         } catch (IOException exc) {
             funcoes.escrever("Exceção ocorrida: " );
